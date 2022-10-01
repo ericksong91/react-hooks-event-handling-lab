446f81c79d29732f53ad5fa543b121fa793121f6
Eric Song <ericksong91@gmail.com>
2022-10-01 16:25:29 -0400

    commit tree 2b235551b69515762d63d78a698f7b2bbd085427

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Entering password...");
+    }
+
     return (
-        <div></div>
+        <div>
+            <form onChange={handleSubmit}>
+                <input type="password" name="keypad" placeholder="Enter password..." />
+                <button>Login</button>
+            </form>
+        </div>
     )
 }
 
